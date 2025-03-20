902e11efa5c9b2774bd2c7bf7a33a30a808d2b68
Aswin warrier <aswinwarrier136@gmail.com>
2025-03-20 14:17:58 +0530

    Added more pages and implemented react-routing

---
+++ react-project/src/router.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router";
+import DashBoard from "./views/DashBoard";
+import Surveys from "./views/Surveys";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <DashBoard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ react-project/src/views/Surveys.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Surveys = () => {
+  return (
+    <>
+      <header className="bg-white shadow-sm">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Surveys
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Surveys;
